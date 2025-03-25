c7058e05bebede924ac280953653dc7c5b366abb
lucas <lucas@lucas-Latitude-E6440>
2025-03-24 18:24:50 -0700

    Merge origin/master
    # Conflicts:
    #	src/Controller/Controlador.java

---
+++ src/main/java/mx/itson/panaderiabda/PanaderiaBDA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.itson.panaderiabda;
+
+/**
+ *
+ * @author lucas
+ */
+public class PanaderiaBDA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
